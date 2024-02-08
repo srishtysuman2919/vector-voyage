d3461c8fc8cf9b62d5573828092a22722edb8da9
Srishty Suman <srishtysuman@Srishtys-MacBook-Pro.local>
2024-02-08 18:34:01 +0530

    chapter3 notebooks

--- 1_LangChainAndVectorDatabases/3_LearningHowToPrompt/quiz.py
+++
@@ -1,4 +0,0 @@
-from langchain import ChatMessagePromptTemplate
-prompt = "May the {subject} be with you"
-chat_message_prompt = ChatMessagePromptTemplate.create_from_template(role="Jedi", template=prompt)
-chat_message_prompt.format(subject="force")
